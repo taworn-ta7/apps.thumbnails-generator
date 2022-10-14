ccef9ed4715bbe92047d42f2c0c65381822e7e0f
Taworn Ta <taworn.ta7@gmail.com>
2022-10-14 16:02:44 +0700

    - rougly finish 4th page

--- thumbnails_generator/src/pages/ProgressPage.vue
+++ thumbnails_generator/src/pages/ProgressPage.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-//import { ref } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
+import { Thumbnail } from '../models/thumbnail'
+//import { useDialogStore } from '../DialogStore'
+import { useAppStore } from '../AppStore'
 
 // using
 const { t } = useI18n({
@@ -19,13 +22,34 @@ const { t } = useI18n({
 	},
 })
 const router = useRouter()
+//const dialogStore = useDialogStore()
+const appStore = useAppStore()
+
+// states
+const images = ref(<Thumbnail[]>[])
+const started = ref(false)
+const finished = ref(false)
+
+// on mounting
+onMounted(() => {
+	images.value = <Thumbnail[]>[]
+	for (let i = 0; i < appStore.images.length; i++)
+		images.value.push(appStore.images[i])
+})
+
+// on unmounting
+onBeforeUnmount(() => {
+	appStore.images = <Thumbnail[]>[]
+	for (let i = 0; i < images.value.length; i++)
+		appStore.images.push(images.value[i])
+})
 
 function onBack() {
 	router.replace('/photo')
 }
 
 function onNext() {
-	console.log(`next round...`)
+	started.value = true
 }
 </script>
 
@@ -33,17 +57,24 @@ function onNext() {
 	<AppBox :title="t('title')">
 		<div class="wrap">
 			<div class="center">
-				progress...
+				<ul>
+					<li v-for="item in images">
+						<div>{{ item.source }}</div>
+						<it-icon name="photo_size_select_large" outlined />
+					</li>
+				</ul>
 			</div>
 
 			<div style="display: flex">
 				<div style="flex: 1 0">
-					<it-button class="page-button" @click.prevent="(e: Event) => onBack()">{{ t('common.back') }}
+					<it-button class="page-button" :disabled="started ? true : null"
+						@click.prevent="(e: Event) => onBack()">{{ t('common.back') }}
 					</it-button>
 				</div>
 				<div>
-					<it-button type="primary" class="page-button" @click.prevent="(e: Event) => onNext()">{{
-					t('common.next') }}</it-button>
+					<it-button type="primary" class="page-button" :disabled="started && !finished ? true : null"
+						@click.prevent="(e: Event) => onNext()">{{
+						!finished ? t('start') : t('restart') }}</it-button>
 				</div>
 			</div>
 		</div>
@@ -52,4 +83,34 @@ function onNext() {
 
 <style scoped>
 @import "../assets/page.css";
+
+ul {
+	margin: 0;
+	padding: 0;
+	display: block;
+	list-style-type: none;
+	width: 100%;
+}
+
+ul>li {
+	margin: 0;
+	padding: 0.5rem;
+	white-space: nowrap;
+	display: flex;
+	flex-wrap: nowrap;
+	align-items: center;
+}
+
+ul>li:nth-of-type(odd) {
+	background-color: #ccc;
+}
+
+ul>li>div {
+	flex: 2 0;
+	align-content: stretch;
+}
+
+ul>li>it-icon {
+	align-self: flex-end;
+}
 </style>
